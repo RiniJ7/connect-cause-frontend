478383617f21f857fb7167c8d4701e096367c4d6
rinij <rinimani7@gmail.com>
2023-04-17 13:23:23 -0600

    jsx file cleaned up header and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,17 @@ import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-//import Footer from "./components/Footer.js";
+import Header from "./Components/header/Header.jsx";
+import Footer from "./Components/footer/Footer.jsx";
 //import SignUp from './Components/SignUp'
 //import SignIn from './Components/SignIn'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    
     <div className="App">
-      {/* <div>
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a> */}
-        {/* <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-        
-      {/* </div> */} 
-
-      <div>
-    
-      <h1>Connect Cause Calgary</h1>
-      <h3>Your next volunteering opportunity is here..</h3>
-      
+      <div >
+      <Header title= "Connect Cause Calgary" subtitle = "Your next voluteering opportunity is here.." choice1 = " LogOut" choice2 = "Home"/>
       </div>
       
 <div className="card">
@@ -38,23 +24,13 @@ function App() {
 </div>
 
 <p className="logo">About Us: How we help you! etc etc</p>
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div> */}
-      {/* <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-  
 
-  {/* <Footer note="Footer Note" /> */}
+<div>
+  <Footer note="Copyrights @ Witty Web Wizards" />
+</div>     
   </div>
+  
  )
-
 }
 
 export default App
