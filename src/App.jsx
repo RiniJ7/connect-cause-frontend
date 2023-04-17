4d714a0ceb35089025fb875225d671269b248274
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-04-17 14:57:34 -0600

    moving signup form function to own component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,22 @@ import "./App.css";
 import Header from "./Components/header/Header.jsx";
 import Footer from "./Components/footer/Footer.jsx";
 import Body from "./Components/body/Body.jsx";
-const volunteerSignupTestObject = {
-    first_name: "Test",
-    last_name: "Lastname",
-    username: "tester",
-    email: "test@gmail.com",
-    password: "notsecurepassword",
-};
-// console.log(JSON.stringify(volunteerSignupTestObject));
+
 function App() {
     const [volunteer, setVolunteer] = useState([]);
 
-    const signUpVolunteer = async () => {
-        const response = await fetch("api/authenticate/signup/volunteer", {
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json",
-            },
-            method: "POST",
-            mode: "cors",
-            body: JSON.stringify(volunteerSignupTestObject),
-        });
-        const volunteerData = await response.json();
-        setVolunteer(volunteerData);
-    };
-
     return (
         <div className="App">
-
-
-          {/* Header  */}
+            {/* Header  */}
             <div>
                 <Header title="Connect Cause Calgary" subtitle="Your next voluteering opportunity is here.." choice1=" LogOut" choice2="Home" />
             </div>
 
+            {/* Body */}
 
+            <Body signin="Sign In" signup="Sign Up" />
 
-          {/* Body */}
-          
-          
-            <Body signin= "Sign In" signup = "Sign Up" /> 
-  
-            
-
-        
-
-          {/* Footer */}
+            {/* Footer */}
             <div>
                 <Footer note="Copyrights @ Witty Web Wizards" />
             </div>
--- src/Components/signup-form/Signupform.jsx
+++ src/Components/signup-form/Signupform.jsx
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Signupform.css";
 
-export default function Signupform(props) {
-  return (
-    <div className="header">
-      <h1>{props.title}</h1>
-      <button className="button">{props.firstname}</button>
-      <button className="button">{props.lastname}</button>
-      <button className="button">{props.username}</button>
-      <button className="button">{props.email}</button>
-      <button className="button">{props.password}</button>      
-    </div>
-  );
+export function Signupform(props) {
+    const [errorMessage, setErrorMessage] = useState();
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const volunteer = {
+        firstName,
+        lastName,
+        username,
+        email,
+        password,
+    };
+
+    const signUpVolunteer = async (event) => {
+        event.preventDefault(); //prevents from rerouting to /? (legacy functionality for sending form data in browsers)
+        try {
+            const response = await fetch("api/authenticate/signup/volunteer", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
+                },
+                body: JSON.stringify(volunteer),
+            });
+            if (!response.ok) {
+                console.log("You encountered an error");
+                const error = await response.json();
+                setErrorMessage(error.message);
+            } else {
+                const volunteerData = await response.json();
+                console.log("response data is: ", volunteerData);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
+    return (
+        <div className="header">
+            <h1>{props.title}</h1>
+            <form onSubmit={signUpVolunteer}>
+                <label>First Name:</label>
+                <input onChange={(event) => setFirstName(event.target.value)} />
+                <label>Last Name:</label>
+                <input onChange={(event) => setLastName(event.target.value)} />
+                <label>Username:</label>
+                <input onChange={(event) => setUsername(event.target.value)} />
+                <label>Email:</label>
+                <input onChange={(event) => setEmail(event.target.value)} />
+                <label>Password:</label>
+                <input onChange={(event) => setPassword(event.target.value)} />
+
+                <input type="submit" title="submit" />
+                {error && <p style={{ color: "red" }}>{errorMessage}</p>}
+            </form>
+
+            {/* <button className="button">{props.firstname}</button>
+            <button className="button">{props.lastname}</button>
+            <button className="button">{props.username}</button>
+            <button className="button">{props.email}</button>
+            <button className="button">{props.password}</button> */}
+        </div>
+    );
 }
