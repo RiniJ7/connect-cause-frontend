b25ca061b440a4171f202f737a0afb669ee40977
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-04-19 12:41:19 -0600

    refactoring the code to fit more in line to how react displays components dynamically

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/App.css";
+
+export default function Footer(props) {
+    return (
+        <div>
+            <p>Copyrights @ Witty Web Wizards"</p>
+        </div>
+    );
+}
