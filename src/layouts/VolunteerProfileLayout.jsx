ba8e2ed617b4a13ab453eb5188cdfd7f1a185457
rinij <rinimani7@gmail.com>
2023-04-27 14:53:30 -0600

    navbar temporarily commented

--- src/layouts/VolunteerProfileLayout.jsx
+++ src/layouts/VolunteerProfileLayout.jsx
@@ -8,9 +8,9 @@ export const VolunteerProfileLayout = () => {
     <div className="volunteer-profile-layout">
       <h2>Volunteer Profile</h2>
       <p>This is a paragraph tag inside the VolunteerProfileLayout file</p>
-      <nav>
+      {/* <nav>
         <NavLink to="/opportunities">View Volunteering Opportunities</NavLink>
-      </nav>
+      </nav> */}
 
       <Outlet />
     </div>
