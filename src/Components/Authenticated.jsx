3dc4f65e7c268692f16242e161fc6e137dff5fc0
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-05-03 10:43:13 -0600

    adding fancy loading animation

--- src/Components/Authenticated.jsx
+++ src/Components/Authenticated.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate, Outlet } from "react-router-dom";
 import { useAuth } from "../providers/AuthProvider";
+import { Jelly } from "@uiball/loaders";
 
 const Authenticated = () => {
   const navigate = useNavigate();
@@ -8,7 +9,14 @@ const Authenticated = () => {
   if (loading) {
     console.log("loading");
 
-    return <div>Loading...</div>;
+    return (
+      <div className="form-group">
+        <h5 className="loading-text">Loading</h5>
+        <br />
+        <br />
+        <Jelly size={100} speed={0.9} color="black" />
+      </div>
+    );
   }
   if (!user) {
     console.log("Authenticated: Did not log in");
