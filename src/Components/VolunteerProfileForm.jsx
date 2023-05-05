aecca5271ddf264fc3358605a72c33ee5bb9da71
Aric Crosson Bouwers <aricbouwers@outlook.com>
2023-05-05 08:20:29 -0600

    fixing error

--- src/Components/VolunteerProfileForm.jsx
+++ src/Components/VolunteerProfileForm.jsx
@@ -22,7 +22,6 @@ export default function VolunteerProfileForm(props) {
     linkedIn,
     profilePicture,
   };
-  console.log(user.userType.firstName)
 
   const createUserProfile = async (event) => {
     event.preventDefault(); //prevents from rerouting to /? (legacy functionality for sending form data in browsers)
@@ -55,96 +54,92 @@ export default function VolunteerProfileForm(props) {
 
   return (
     <div className="width-style">
-    <div className="profilepage-container">
-      <main>
-        <div>
-          <form className="form" onSubmit={createUserProfile}>
-            <h3>{props.title}</h3>
-            <br />
+      <div className="profilepage-container">
+        <main>
+          <div>
+            <form className="form" onSubmit={createUserProfile}>
+              <h3>{props.title}</h3>
+              <br />
+
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  placeholder="Enter your first name"
+                  name="firstName"
+                  value={firstName}
+                  required
+                  onChange={(event) => setFirstName(event.target.value)}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  placeholder="Enter your last name"
+                  name="lastName"
+                  value={lastName}
+                  required
+                  onChange={(event) => setLastName(event.target.value)}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="About Me">About Me</label>
+                <input
+                  type="text"
+                  id="aboutMe"
+                  placeholder="Tell about yourself"
+                  name="aboutMe"
+                  value={aboutMe}
+                  required
+                  onChange={(event) => setAboutMe(event.target.value)}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="Profile Picture">Profile Picture</label>
+                <input
+                  type="text"
+                  id="profilePicture"
+                  placeholder="Please paste the file path of your image"
+                  name="profilePicture"
+                  value={profilePicture}
+                  onChange={(event) => setProfilePicture(event.target.value)}
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="LinkedIn">LinkedIn</label>
+                <input
+                  type="text"
+                  id="LinkedIn"
+                  placeholder="Please paste the file path of your LinkedIn"
+                  name="LinkedIn"
+                  value={linkedIn}
+                  onChange={(event) => setLinkedIn(event.target.value)}
+                />
+              </div>
+
+              <div>
+                <AllInterests />
+              </div>
+            </form>
 
             <div className="form-group">
-              <label htmlFor="firstName">First Name</label>
-              <input
-                type="text"
-                id="firstName"
-                placeholder="Enter your first name"
-                name="firstName"
-                value={firstName}
-                required
-                onChange={(event) => setFirstName(event.target.value)}
-              />
+              {/* // what does this do */}
+              <Button className="form-submit-btn" type="submit" onClick={createUserProfile}>
+                Submit your profile
+              </Button>
+
+              <br />
             </div>
-
-            <div className="form-group">
-              <label htmlFor="lastName">Last Name</label>
-              <input
-                type="text"
-                id="lastName"
-                placeholder="Enter your last name"
-                name="lastName"
-                value={lastName}
-                required
-                onChange={(event) => setLastName(event.target.value)}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="About Me">About Me</label>
-              <input
-                type="text"
-                id="aboutMe"
-                placeholder="Tell about yourself"
-                name="aboutMe"
-                value={aboutMe}
-                required
-                onChange={(event) => setAboutMe(event.target.value)}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="Profile Picture">Profile Picture</label>
-              <input
-                type="text"
-                id="profilePicture"
-                placeholder="Please paste the file path of your image"
-                name="profilePicture"
-                value={profilePicture}
-                onChange={(event) => setProfilePicture(event.target.value)}
-              />
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="LinkedIn">LinkedIn</label>
-              <input
-                type="text"
-                id="LinkedIn"
-                placeholder="Please paste the file path of your LinkedIn"
-                name="LinkedIn"
-                value={linkedIn}
-                onChange={(event) => setLinkedIn(event.target.value)}
-              />
-            </div>
-
-            <div>
-              <AllInterests />
-            </div>
-          </form>
-
-          <div className="form-group">
-            {/* // what does this do */}
-            <Button
-              className="form-submit-btn"
-              type="submit"
-              onClick={createUserProfile}
-            >
-              Submit your profile
-            </Button>
-
-            <br />
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
     </div>
   );
 }
